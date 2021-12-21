8365805ba29f781d2289bab2228dba6e6d477195
yadonx <ej_yo@hotmail.com>
2021-12-21 16:43:35 +0100

    added Cross Origin

--- src/main/java/com/example/examensarbete/Controller/MovieController.java
+++ src/main/java/com/example/examensarbete/Controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/movie")
--- src/main/java/com/example/examensarbete/Controller/SeriesController.java
+++ src/main/java/com/example/examensarbete/Controller/SeriesController.java
@@ -7,13 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/serie")
